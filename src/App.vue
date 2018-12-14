b13dbe1173c07040fdcc07a948040223dcad0a60
adowning <ashdowning@gmail.com>
2018-12-13 21:16:16 -0600

    erer

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,32 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'app',
 
-  components: {}
+  components: {},
+  mounted() {
+    const options = {
+      client_id: '0cbaa9173943569cad4c0b8267981147bac0cf5d',
+      client_secret: 'be6a34d0830ab6fb3db837958d50faace249e0d1',
+      grant_type: 'password',
+      username: 'ash@andrewscarpetcleaning.com',
+      password: 'sugarlips42'
+    }
+
+    axios
+      .post('/token', options)
+      .then(response => {
+        console.log('1')
+        console.log(response.data.access_token)
+      })
+      .catch(err => {
+        console.log('2')
+        console.log(err)
+      })
+  }
 }
 </script>
 
